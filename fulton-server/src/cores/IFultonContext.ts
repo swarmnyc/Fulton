8a7c48c7f8f3ceeada0c503c2dae27a22146b75f
Wade <wadehuang36@gmail.com>
2017-12-01 13:00:44 -0500

    update interface

--- fulton-server/src/cores/IFultonContext.ts
+++ fulton-server/src/cores/IFultonContext.ts
@@ -5,8 +5,13 @@ import { IUserManager } from "../index";
 import { IContainer } from "../../node_modules/tsioc";
 import { FultonApp } from "../FultonApp";
 
-export interface IFultonContext<TUser extends IUser = IUser> extends Context {
-    fultonApp: FultonApp;
+export interface IFultonContext<TUser extends IUser = IUser> {
+    user: TUser;
+    logger: ILogger;
+    container: IContainer;
+}
+
+export interface IFultonRouterContext<TUser extends IUser = IUser> extends Context {
     user: TUser;
     logger: ILogger;
     container: IContainer;

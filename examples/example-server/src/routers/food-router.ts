f2c684758e0f35189f6dabe2ad24a728c7564dbe
Wade <wadehuang36@gmail.com>
2018-02-15 20:01:18 -0500

    remove example

--- examples/example-server/src/routers/food-router.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRouter, Router, Middleware, Request, Response, injectable, httpGet, router, inject, authorize, authorizeByRole, EntityService, entityRouter } from "fulton-server"
-import { Food } from "../entities/food";
-
-@entityRouter(["api", /foods?/], Food, authorize(), authorizeByRole("admin"))
-export class FoodRouter extends EntityRouter<Food> {
-}
--- examples/example-server/src/routers/ingredient-router.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRouter, Router, Middleware, Request, Response, injectable, httpGet, router, inject, authorize, authorizeByRole, EntityService, entityRouter } from "fulton-server"
-import { Food } from "../entities/food";
-import { Ingredient } from "../entities/ingredient";
-
-@entityRouter("/api/ingredients", Ingredient)
-export class IngredientRouter extends EntityRouter<Ingredient> {
-}

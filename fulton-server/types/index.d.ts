0caa132fffd3754e75a2ea2c8bfb100ed6923624
Wade <wadehuang36@gmail.com>
2018-05-15 18:36:49 -0400

    fix type errors

--- fulton-server/types/index.d.ts
+++ fulton-server/types/index.d.ts
@@ -1,6 +1,18 @@
 import { Column, Entity, ManyToMany, ManyToOne, ObjectIdColumn, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
-// this file if typescript supports, because I want to make typeorm package is optional
+export * from "../main"
+
+export const column: typeof Column;
+export const entity: typeof Entity;
+export const manyToMany: typeof ManyToMany;
+export const manyToOne: typeof ManyToOne;
+export const objectIdColumn: typeof ObjectIdColumn;
+export const oneToMany: typeof OneToMany;
+export const oneToOne: typeof OneToOne;
+export const primaryColumn: typeof PrimaryColumn;
+export const primaryGeneratedColumn: typeof PrimaryGeneratedColumn;
+
+export { ObjectId } from 'bson';
 
 declare module "../src/re-export" {
     export const column: typeof Column;
@@ -15,4 +27,3 @@ declare module "../src/re-export" {
     
     export { ObjectId } from 'bson';
 }
-
